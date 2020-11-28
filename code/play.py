0f4e24ab2c56988c252e3b8a314aa626cb96310c
Marten L Thompson <thom7058@umn.edu>
2020-11-28 17:06:46 -0600

    QvQ grid run mostly complete; logging in final/QvQ. Visualizing learning almost complete; logging in final/Qvis

--- code/play.py
+++ code/play.py
@@ -1,4 +1,4 @@
-# Methods to conduct a single game of nim
+# Primary script in project
 
 from agents import QAgent
 from agents import PerfectAgent
@@ -7,12 +7,12 @@ from statesActions import de_hash
 
 import csv
 from datetime import datetime
-
-import sys
 import numpy.random as rnd
 import numpy as np
+import pandas as pd
+import sys
 
-# board should be list of ints 0-9
+
 def play_nim(p1, p2, board_hash, win_reward, lose_reward):
     """
     Simulate a single complete game between two agents. 
@@ -22,8 +22,6 @@ def play_nim(p1, p2, board_hash, win_reward, lose_reward):
     if sum(de_hash(board_hash)) <= 0:
         sys.exit('Illegal starting board')
     
-    # print('Starting board:', board)
-    
     turns = 0
     while True: 
         if 0 == turns:
@@ -31,11 +29,9 @@ def play_nim(p1, p2, board_hash, win_reward, lose_reward):
         else:
             new_board_hash = p1.move(new_board_hash)
             
-        #print('P1:', new_board_hash)
-        
+       
         if 0 == sum(de_hash(new_board_hash)):
             # P1 wins
-            # print('P1 won!')
             p1.learn(1, win_reward)
             
             if 0 != turns:
@@ -45,11 +41,9 @@ def play_nim(p1, p2, board_hash, win_reward, lose_reward):
             break
         
         new_board_hash = p2.move(new_board_hash)
-        #print('P2:', new_board_hash)
         
         if 0 == sum(de_hash(new_board_hash)):
             # P2 wins
-            # print('P2 won!')
             p1.learn(0, lose_reward)
             p2.learn(1, win_reward)
             break
@@ -59,27 +53,17 @@ def play_nim(p1, p2, board_hash, win_reward, lose_reward):
         p2.learn(None, 0)
 
         turns += 1
-        #print('round complete')
         
-    #print('game over')
+    # game over
 
 
 def train_agents(n_games, p1, p2, starting_board_hash, win_reward, lose_reward, trace):
     """
-    for i in range(n_games):
-        p1.started = True
-        p2.started = False
-        
-        if i > 900:
-            print('debug')
-        play_nim(p1, p2, starting_board, 1, -1)
-    
+    Play n_games many games of Nim using starting_board_hash. Randomly choose
+    who plays first with equal probability.
     """
     
-    for i in range(n_games):
-        #if i %10000 == 0:
-        #    print(i)
-        
+    for i in range(n_games):    
         if rnd.uniform(0,1,1)[0] > 0.5: 
             p1.started = True
             p2.started = False
@@ -96,12 +80,17 @@ def train_agents(n_games, p1, p2, starting_board_hash, win_reward, lose_reward, 
     return([[p1.optimal_per_start, p1.winlose_per_start], [p2.optimal_per_start, p2.winlose_per_start]])
 
 
-def main():
-    N_GAMES = 75000 # int(5e3)
+def QvQgrid():
+    """
+    Simulate 10 repetitions of 75000 training games between two Q-learners 
+    across a grid of hyperparameters. Write out perc. optimal moves and
+    win/lose rates for each game an agent was player 1.
+    """
+    rnd.seed(513)
+    
+    N_GAMES = 75000 
     REPS = 5
     Q_init = 0.0
-    
-    rnd.seed(513)
     
     epsilons = [0.1,0.3,0.5,0.7,0.9]
     epsilons = [0.7,0.9]
@@ -109,8 +98,6 @@ def main():
     # alphas = [0.1]
     gammas = [-0.1,-0.3,-0.5,-0.7,-0.9]
     # gammas = [-0.1]
-    # starting_board = get_hash(rnd.randint(0,9,3).tolist())
-    # starting_board_hash = get_hash([4,5,8])
     
     setting = [[e,a,g] for e in epsilons for a in alphas for g in gammas] 
     
@@ -134,9 +121,7 @@ def main():
             else:
                 starting_board_hash = get_hash([4,4,4]) # one in million chance this will be needed
             
-            #p1 = QAgent('p102', num_piles, starting_board, Q_init, 0.1) 
             p1 = QAgent('p1', starting_board_hash, Q_init, epsilon, alpha, gamma)
-            #p2 = PerfectAgent('perfect')
             p2 = QAgent('p2', starting_board_hash, Q_init, epsilon, alpha, gamma)
             
             [p1_stats, p2_stats] = train_agents(N_GAMES, p1, p2, starting_board_hash, 1, -1, False)
@@ -149,7 +134,6 @@ def main():
         file = open(filename, 'w+', newline ='')
         file_contents = p1_opt_percs + p2_opt_percs
     
-        
         with file:
             write = csv.writer(file)
             write.writerows(file_contents)
@@ -157,7 +141,6 @@ def main():
         filename = 'final/' + 'wins' + str(epsilon) + str(alpha) + str(gamma) +'vSelfAll_'+ datetime.now().strftime("%Y_%m_%d-%H_%M_%S") + '.csv'
         file = open(filename, 'w+', newline ='')
         file_contents = p1_winlose + p2_winlose
-    
         
         with file:
             write = csv.writer(file)
@@ -165,5 +148,64 @@ def main():
     
     print('learning complete')
 
+
+def vis_learning():
+    """
+    Write out Q table for agent as it learns.
+    """
+    rnd.seed(444)
+    
+    N_GAMES = 1000
+    
+    starting_board_hash = get_hash([2,2])
+    p1 = QAgent('p1', starting_board_hash, 0.0, 0.1, 0.3, -0.5)
+    p2 = QAgent('p2', starting_board_hash, 0.0, 0.1, 0.3, -0.5)
+    
+    # values in Q table over time
+    Qseries = pd.DataFrame(columns=['01-00', 
+                                    '02-00', '02-01',
+                                    '10-00',
+                                    '11-01', '11-10',
+                                    '12-02', '12-10', '12-11',
+                                    '20-00', '20-10',
+                                    '21-01', '21-11', '21-20',
+                                    '22-02', '22-12', '22-20', '22-21'])
+    
+    
+    for i in range(N_GAMES):
+        play_nim(p1, p2, starting_board_hash, 1, -1)
+        temp = []
+        temp.append(p1.Q['0, 1']['0, 1'])
+        temp.append(p1.Q['0, 2']['0, 2'])
+        temp.append(p1.Q['0, 2']['0, 1'])
+        temp.append(p1.Q['1, 0']['1, 0'])    
+        temp.append(p1.Q['1, 1']['1, 0'])
+        temp.append(p1.Q['1, 1']['0, 1'])        
+        temp.append(p1.Q['1, 2']['1, 0'])
+        temp.append(p1.Q['1, 2']['0, 2'])
+        temp.append(p1.Q['1, 2']['0, 1'])
+        temp.append(p1.Q['2, 0']['2, 0'])
+        temp.append(p1.Q['2, 0']['1, 0'])
+        temp.append(p1.Q['2, 1']['2, 0'])
+        temp.append(p1.Q['2, 1']['1, 0'])
+        temp.append(p1.Q['2, 1']['0, 1'])
+        temp.append(p1.Q['2, 2']['2, 0'])
+        temp.append(p1.Q['2, 2']['1, 0'])
+        temp.append(p1.Q['2, 2']['0, 2'])
+        temp.append(p1.Q['2, 2']['0, 1'])
+        
+        Qseries.loc[i] = temp
+    
+    
+    Qseries.to_csv('final/Qvis/Qseries.csv', index=False)
+    print('complete')
+    
+    
+    
+    
+
+
 if __name__ == "__main__":
-    main()
+    # QvQgrid()
+    # BestvRand()
+    vis_learning()
